db46a545b496ef746d45fd6bad91fe10a7a7d964
Lekhraj Panjwani <67046356+lekhrajpanjwani@users.noreply.github.com>
2022-04-30 12:52:50 +0530

    Update oktaConfig.js
    remove keys

--- spa/src/lib/oktaConfig.js
+++ spa/src/lib/oktaConfig.js
@@ -3,9 +3,9 @@ const REACT_APP_OKTA_DOMAIN = process.env.REACT_APP_OKTA_DOMAIN;
 const REACT_APP_PORT = process.env.REACT_APP_PORT;
 
 export const oktaConfig = {
-  clientId: `0oa4v41iylQIeSPYm5d7`,
-  issuer: `https://dev-59276804.okta.com/oauth2/default`,
-  redirectUri: `http://localhost:3000/login/callback`, // this makes it so redirects to login if not logged in for secure routes
+  clientId: `${REACT_APP_CLIENT_ID}`,
+  issuer: `https://${REACT_APP_OKTA_DOMAIN}/oauth2/default`,
+  redirectUri: `http://localhost:${REACT_APP_PORT}/login/callback`, // this makes it so redirects to login if not logged in for secure routes
   scopes: ["openid", "profile", "email"],
   pkce: true,
   disableHttpsCheck: true,
